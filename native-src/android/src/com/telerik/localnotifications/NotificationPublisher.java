fa92b3e609133a7a1c0f51683ab0df58ce3b9abb
Ziggy <Z@Obsessive.tech>
2016-09-15 10:58:45 +0530

    Thx ben :D

--- native-src/android/src/com/telerik/localnotifications/NotificationPublisher.java
+++ native-src/android/src/com/telerik/localnotifications/NotificationPublisher.java
@@ -1,20 +1,44 @@
+/*
+ * Decompiled with CFR 0_115.
+ * 
+ * Could not load the following classes:
+ *  android.app.Notification
+ *  android.app.NotificationManager
+ *  android.content.BroadcastReceiver
+ *  android.content.Context
+ *  android.content.Intent
+ *  android.net.Uri
+ *  android.os.Parcelable
+ */
 package com.telerik.localnotifications;
-import android.net.Uri;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Parcelable;
+import android.util.Log;
+import android.widget.Toast;
 
-public class NotificationPublisher extends BroadcastReceiver {
+import static android.R.attr.data;
 
-  public static String NOTIFICATION_ID = "notification-id";
-  public static String NOTIFICATION = "notification";
+public class NotificationPublisher
+extends BroadcastReceiver {
+    public static String NOTIFICATION_ID = "notification-id";
+    public static String NOTIFICATION = "notification";
+    public static String SOUND = "sound";
 
-  public void onReceive(Context context, Intent intent) {
-    final Notification notification = intent.getParcelableExtra(NOTIFICATION);
-    notification.sound = Uri.parse("android.resource://" + context.getPackageName() + "/raw/notify");
-    final int id = intent.getIntExtra(NOTIFICATION_ID, 0);
-    ((NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE)).notify(id, notification);
-  }
+    public void onReceive(Context context, Intent intent) {
+        Notification notification = (Notification)intent.getParcelableExtra(NOTIFICATION);
+        Uri d_sound = Uri.parse((String)("android.resource://" + context.getPackageName() + "/raw/" + notification.sound));
+
+       // Toast.makeText(context,notification.toString() , Toast.LENGTH_LONG).show();
+       // Toast.makeText(context,d_sound.toString() , Toast.LENGTH_LONG).show();
+         notification.sound = d_sound;
+        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+        ((NotificationManager)context.getSystemService("notification")).notify(id, notification);
+    }
 }
+
--- native-src/android/src/com/telerik/localnotifications/NotificationRestoreReceiver.java
+++ native-src/android/src/com/telerik/localnotifications/NotificationRestoreReceiver.java
@@ -1,3 +1,23 @@
+/*
+ * Decompiled with CFR 0_115.
+ * 
+ * Could not load the following classes:
+ *  android.app.AlarmManager
+ *  android.app.Notification
+ *  android.app.NotificationManager
+ *  android.app.PendingIntent
+ *  android.content.BroadcastReceiver
+ *  android.content.Context
+ *  android.content.Intent
+ *  android.content.SharedPreferences
+ *  android.net.Uri
+ *  android.os.Parcelable
+ *  android.support.v4.app.NotificationCompat
+ *  android.support.v4.app.NotificationCompat$Builder
+ *  android.util.Log
+ *  org.json.JSONException
+ *  org.json.JSONObject
+ */
 package com.telerik.localnotifications;
 
 import android.app.AlarmManager;
@@ -9,108 +29,68 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import android.support.v4.app.NotificationCompat.Builder;
+import android.os.Parcelable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.Date;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * Notifications need to be restored when the device is rebooted,
- * that's what's this class is for.
- */
-public class NotificationRestoreReceiver extends BroadcastReceiver {
+public class NotificationRestoreReceiver
+extends BroadcastReceiver {
+    static final String TAG = "NotificationRestoreReceiver";
 
-  static final String TAG = "NotificationRestoreReceiver";
+    public void onReceive(Context context, Intent intent) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LocalNotificationsPlugin", 0);
+        Set<? extends Map.Entry<?,?>> notificationOptions = sharedPreferences.getAll().entrySet();
 
-  @Override
-  public void onReceive(Context context, Intent intent) {
-    final SharedPreferences sharedPreferences = context.getSharedPreferences("LocalNotificationsPlugin", Context.MODE_PRIVATE);
-    final Set notificationOptions = sharedPreferences.getAll().entrySet();
+        for (Map.Entry item : notificationOptions) {
+            String notificationId = (String)item.getKey();
+            String notificationString = (String)item.getValue();
+            try {
+                JSONObject options = new JSONObject(notificationString);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                        .setDefaults(0)
+                        .setContentTitle((CharSequence)options.optString("title"))
+                        .setContentText((CharSequence)options.optString("body"))
+                        .setSmallIcon(options.optInt("icon")).setAutoCancel(true)
+                        .setSound(options.has("sound") ? Uri.parse((String)("android.resource://" + context.getPackageName() + "/raw/" + options.optString("sound"))) : Uri.parse((String)("android.resource://" + context.getPackageName() + "/raw/notify")) )
+                        .setNumber(options.optInt("badge"))
+                        .setTicker((CharSequence)options.optString("ticker"));
 
-    Iterator<Map.Entry> iterator = notificationOptions.iterator();
-    while (iterator.hasNext()) {
-      Map.Entry item = iterator.next();
-      String notificationId = (String) item.getKey();
-      String notificationString = (String) item.getValue();
-      try {
-        JSONObject options = new JSONObject(notificationString);
-        
-        final Builder builder = new Builder(context)
-            .setDefaults(0)
-            .setContentTitle(options.optString("title"))
-            .setContentText(options.optString("body"))
-            .setSmallIcon(options.optInt("icon"))
-            .setAutoCancel(true)
-            .setSound(options.has("sound") ? Uri.parse((String)("android.resource://" + context.getPackageName() + "/raw/" + options.optString("sound"))) : Uri.parse((String)("android.resource://" + context.getPackageName() + "/raw/notify"))) 
-            .setNumber(options.optInt("badge"))
-            .setTicker(options.optString("ticker"));
+                //Toast.makeText(context, "Custom sound name: " + options.optString("sound") , Toast.LENGTH_LONG).show();
 
-        // add the intent that handles the event when the notification is clicked (which should launch the app)
-        final Intent clickIntent = new Intent(context, NotificationClickedActivity.class)
-            .putExtra("pushBundle", notificationString)
-            .setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-
-        final PendingIntent pendingContentIntent = PendingIntent.getActivity(context, options.optInt("id")+100, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(pendingContentIntent);
-
-        final Notification notification = builder.build();
-
-        // add the intent which schedules the notification
-        final Intent notificationIntent = new Intent(context, NotificationPublisher.class)
-            .setAction(options.getString("id"))
-            .putExtra(com.telerik.localnotifications.NotificationPublisher.NOTIFICATION_ID, options.optInt("id"))
-            .putExtra(com.telerik.localnotifications.NotificationPublisher.NOTIFICATION, notification);
-
-        final PendingIntent pendingIntent = PendingIntent.getBroadcast(context, options.optInt("id")+200, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-
-        // configure when we'll show the event
-        long triggerTime = options.getLong("atTime");
-        final Date triggerDate = new Date(triggerTime);
-        final boolean wasInThePast = new Date().after(triggerDate);
-        final AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        if (wasInThePast) {
-          Log.d(TAG, "------------------------ cancel notification: " + options);
-          alarmManager.cancel(pendingIntent);
-          ((NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE)).cancel(options.getInt("id"));
-          // TODO 'unpersist' would be nice
-        } else {
-          // schedule
-          Log.d(TAG, "------------------------ schedule notification: " + options);
-          alarmManager.set(AlarmManager.RTC_WAKEUP, triggerTime, pendingIntent);
+                Intent clickIntent = new Intent(context, (Class)NotificationClickedActivity.class).putExtra("pushBundle", notificationString).setFlags(1073741824);
+                PendingIntent pendingContentIntent = PendingIntent.getActivity((Context)context, (int)(options.optInt("id") + 100), (Intent)clickIntent, (int)134217728);
+                builder.setContentIntent(pendingContentIntent);
+                Notification notification = builder.build();
+                Intent notificationIntent = new Intent(context, (Class)NotificationPublisher.class).setAction(options.getString("id"))
+                        .putExtra(NotificationPublisher.NOTIFICATION_ID, options.optInt("id"))
+                        .putExtra(NotificationPublisher.SOUND, options.optString("sound"))
+                        .putExtra(NotificationPublisher.NOTIFICATION, (Parcelable)notification);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast((Context)context, (int)(options.optInt("id") + 200), (Intent)notificationIntent, (int)268435456);
+                long triggerTime = options.getLong("atTime");
+                Date triggerDate = new Date(triggerTime);
+                boolean wasInThePast = new Date().after(triggerDate);
+                AlarmManager alarmManager = (AlarmManager)context.getSystemService("alarm");
+                if (wasInThePast) {
+                    Log.d((String)"NotificationRestoreReceiver", (String)("------------------------ cancel notification: " + (Object)options));
+                    alarmManager.cancel(pendingIntent);
+                    ((NotificationManager)context.getSystemService("notification")).cancel(options.getInt("id"));
+                    continue;
+                }
+                Log.d((String)"NotificationRestoreReceiver", (String)("------------------------ schedule notification: " + (Object)options));
+                alarmManager.set(0, triggerTime, pendingIntent);
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
-      } catch (JSONException e) {
-        e.printStackTrace();
-      }
     }
-  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
